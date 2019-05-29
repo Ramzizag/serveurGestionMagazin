6b6958b65b7c6be25e3624d7809c38bbc966db02
Ramzizag <46466620+Ramzizag@users.noreply.github.com>
2019-05-29 14:05:19 +0100

    first commit

---
+++ eclipse-workspace/serveurGestionMagazin/src/main/java/com/example/demo/ServeurGestionMagazinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionMagazinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionMagazinApplication.class, args);
+	}
+
+}
